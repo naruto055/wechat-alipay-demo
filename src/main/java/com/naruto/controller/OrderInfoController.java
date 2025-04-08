570306e333f3eab22d77e92a4cdc46e0209391d9
naruto055 <1935994680@qq.com>
2025-04-08 23:31:15 +0800

    feat: 查询订单状态接口

--- src/main/java/com/naruto/controller/OrderInfoController.java
+++ src/main/java/com/naruto/controller/OrderInfoController.java
@@ -1,14 +1,12 @@
 package com.naruto.controller;
 
 
+import com.naruto.enums.OrderStatus;
 import com.naruto.model.entity.OrderInfo;
 import com.naruto.model.vo.R;
 import com.naruto.service.OrderInfoService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -31,5 +29,20 @@ public class OrderInfoController {
     public R list() {
         List<OrderInfo> list = orderInfoService.listOrderByCreateTimeDesc();
         return R.ok().data("list", list);
+    }
+
+    /**
+     * 查询订单状态
+     *
+     * @param orderNo
+     * @return
+     */
+    @GetMapping("/query-order-status/{orderNo}")
+    public R getOrderStatus(@PathVariable String orderNo) {
+        String orderStatus = orderInfoService.getOrderStatus(orderNo);
+        if (OrderStatus.SUCCESS.getType().equals(orderStatus)) {
+            return R.ok().setMessage("支付成功");
+        }
+        return R.ok().setCode(101).setMessage("支付中......");
     }
 }
